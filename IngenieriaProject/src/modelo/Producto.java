abc210a70d9b3aa7620e266163071e1a3ddf835d
mateozamora <zamoragamboal@gmail.com>
2025-11-18 08:37:49 -0500

    k

--- IngenieriaProject/src/modelo/Producto.java
+++ IngenieriaProject/src/modelo/Producto.java
@@ -5,7 +5,7 @@ public class Producto {
     private String sku;
     private int cantidad;
     private double precio;
-    private static int contadorProductos = 0;
+   private static int contarProductos = 0;
 
     public Producto(String nombre, String sku, int cantidad, double precio) {
         this.nombre = nombre;
