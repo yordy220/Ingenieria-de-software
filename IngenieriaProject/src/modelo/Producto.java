f0898724f151e531efdbd8a6ad6d4d836d8dcf5e
mateozamora <zamoragamboal@gmail.com>
2025-11-18 08:27:45 -0500

    cara

--- IngenieriaProject/src/modelo/Producto.java
+++ IngenieriaProject/src/modelo/Producto.java
@@ -5,12 +5,14 @@ public class Producto {
     private String sku;
     private int cantidad;
     private double precio;
+    private static int contadorProductos = 0;
 
     public Producto(String nombre, String sku, int cantidad, double precio) {
         this.nombre = nombre;
         this.sku = sku;
         this.cantidad = cantidad;
         this.precio = precio;
+
     }
 
     // Getters y Setters
